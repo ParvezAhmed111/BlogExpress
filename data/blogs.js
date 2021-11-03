9ac7da1f8953cc4dc5f69203a104c12028d8c3e0
Sheikh Parvez Ahmed <parvezahmed.0947@gmail.com>
2021-11-03 13:16:04 +0530

    Initial Commit

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+
+blogs=[
+    {
+        title: "How to get started with Python",
+        content: "This is content Python ",
+        slug: "python-learn"
+    },
+    {
+        title: "How to get started with JS",
+        content: "This is content JS",
+        slug: "JS-learn"
+    },
+    {
+        title: "How to get started with Django",
+        content: "This is content Django",
+        slug: "Django-learn"
+    },
+    {
+        title: "How to get started with CSS",
+        content: "This is content CSS",
+        slug: "CSS-learn"
+    },
+]
+
+module.exports= blogs;
